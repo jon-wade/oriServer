99a95d4d6e8c596d350bffbfddc110f8bfc3e888
jon-wade <jonwadeuk@gmail.com>
2020-03-07 13:01:44 +0000

    add unit test for summation method

--- server/mathHelper_test.go
+++ server/mathHelper_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jon-wade/oriServer/server"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"math"
 	"testing"
 )
 
@@ -71,5 +72,65 @@ func TestMathHelperServer_Factorial(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestMathHelperServer_Summation(t *testing.T) {
+	var tests = []struct {
+		in     *pb.SummationInput
+		result *pb.SummationResult
+		err    error
+	}{
+		{
+			&pb.SummationInput{First: 2, Last: 2},
+			&pb.SummationResult{Result: 4, First: 2, Last: 2},
+			nil,
+		},
+		{
+			&pb.SummationInput{First: 250, Last: -250},
+			&pb.SummationResult{Result: 0, First: 250, Last: -250},
+			nil,
+		},
+		{
+			&pb.SummationInput{First: math.MaxInt64, Last: 1},
+			nil,
+			status.Errorf(codes.OutOfRange, "summation result exceeds maximum integer value"),
+		},
+	}
+
+	s := server.MathHelperServer{}
+	ctx := context.Background()
+
+	for idx, testData := range tests {
+		testName := fmt.Sprintf("in=%v,result=%v,err=%v", testData.in, testData.result, testData.err)
+		t.Run(testName, func(t *testing.T) {
+			result, err := s.Summation(ctx, testData.in)
+
+			switch idx {
+			case 0, 1:
+				if err != testData.err {
+					t.Errorf("Expected err=%v, got %v", testData.err, err)
+				}
+				if err == nil {
+					if result.First != testData.result.First {
+						t.Errorf("Expected result.First=%v, got %v", testData.result.First, result.First)
+					}
+					if result.Last != testData.result.Last {
+						t.Errorf("Expected result.Last=%v, got %v", testData.result.Last, result.Last)
+					}
+					if result.Result != testData.result.Result {
+						t.Errorf("Expected result.Base=%v, got %v", testData.result.Result, result.Result)
+					}
+				}
+			case 2:
+				if result != testData.result {
+					t.Errorf("Expected result=%v, got %v", testData.result, result)
+				}
+				if err != nil {
+					if err.Error() != testData.err.Error() {
+						t.Errorf("Expected err=%v, got %v", testData.err, err)
+					}
+				}
+			}
+		})
+	}
 }
